baca0071cea09af4f6cddd9a8d7f43ec51344f38
austin <austinw.msg@gmail.com>
2016-06-25 16:04:38 -0700

    removed main.go
    it was used as a reference but is no longer needed

--- cryptobox.go
+++ cryptobox.go
@@ -47,4 +47,66 @@ func getCryptoValues(json string) (string, string) {
 	return ethusd, btcusd
 }
 
+func tick() {
 
+	bl.Root()
+	{
+		bl.Pos(64,64)
+		bl.Dim(800,600)
+		bl.Color(.3,.5,.5)
+		bl.Flag(bl.Z_COLOR_SOLID | bl.Z_BORDER_ALL)
+
+		bl.Font("arial", 6)
+		bl.FontColor(1,1,1)
+		bl.FontNudge(3,3)
+		bl.Label("Hello world")
+
+		bl.BorderThickness([]int32{2,2,2,2})
+		bl.BorderColor(1,1,1)
+
+		bl.On("hover", func(i interface{}){
+			e := i.(*mouse_hover.Event)
+
+			if e.IsInEvent {
+			}
+		})
+
+		bl.Div()
+		{
+			bl.ID("red")
+			bl.Pos(60, 60)
+			bl.Dim(164,148)
+			bl.Color(.1,0,.0)
+			bl.BorderThickness([]int32{1,1,1,1})
+			bl.BorderColor(1,1,1)
+			bl.BorderTopsCanvas()
+
+			bl.On("hover", func(i interface{}){
+				e := i.(*mouse_hover.Event)
+
+				if e.IsInEvent {
+				}
+			})
+
+			button.ID("btn", func() {
+				fmt.Println("wow")
+			})
+			{
+				//button.Label("SHazzy")
+				//button.OnClick(func() {
+				//	fmt.Println("click")
+				//})
+			}
+			//button.End()
+		}
+		bl.End()
+	}
+	bl.End()
+}
+
+func uninit() {
+}
+
+func init() {
+	runtime.LockOSThread()
+}
