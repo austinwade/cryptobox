8f26b64dbea817d61baadb8b81e239d721fb48d5
austin <austinw.msg@gmail.com>
2016-06-25 21:45:12 -0700

    added Bellina window open
    the info needs a window to be displayed on

--- cryptobox.go
+++ cryptobox.go
@@ -5,19 +5,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"runtime"
 	"github.com/amortaza/go-bellina"
 )
-
-func main() {
-	json := getJSONFromApi()
-
-	ethusd, btcusd := getCryptoValues(json)
-
-	bl.Start( 1024, 768, "Bellina v0.2", init_, tick, uninit )
-
-	fmt.Println("btc: " + btcusd)
-	fmt.Println("eth: " + ethusd)
-}
 
 func getJSONFromApi() string {
 	var apiUrl string = "https://api.etherscan.io/api?module=stats&action=ethprice"
@@ -54,15 +44,6 @@ func tick() {
 		bl.Pos(64,64)
 		bl.Dim(800,600)
 		bl.Color(.3,.5,.5)
-		bl.Flag(bl.Z_COLOR_SOLID | bl.Z_BORDER_ALL)
-
-		bl.Font("arial", 6)
-		bl.FontColor(1,1,1)
-		bl.FontNudge(3,3)
-		bl.Label("Hello world")
-
-		bl.BorderThickness([]int32{2,2,2,2})
-		bl.BorderColor(1,1,1)
 
 		bl.On("hover", func(i interface{}){
 			e := i.(*mouse_hover.Event)
@@ -71,35 +52,6 @@ func tick() {
 			}
 		})
 
-		bl.Div()
-		{
-			bl.ID("red")
-			bl.Pos(60, 60)
-			bl.Dim(164,148)
-			bl.Color(.1,0,.0)
-			bl.BorderThickness([]int32{1,1,1,1})
-			bl.BorderColor(1,1,1)
-			bl.BorderTopsCanvas()
-
-			bl.On("hover", func(i interface{}){
-				e := i.(*mouse_hover.Event)
-
-				if e.IsInEvent {
-				}
-			})
-
-			button.ID("btn", func() {
-				fmt.Println("wow")
-			})
-			{
-				//button.Label("SHazzy")
-				//button.OnClick(func() {
-				//	fmt.Println("click")
-				//})
-			}
-			//button.End()
-		}
-		bl.End()
 	}
 	bl.End()
 }
@@ -107,6 +59,17 @@ func tick() {
 func uninit() {
 }
 
-func init() {
+func init_() {
 	runtime.LockOSThread()
+}
+
+func main() {
+	json := getJSONFromApi()
+
+	ethusd, btcusd := getCryptoValues(json)
+
+	bl.Start( 1024, 768, "Bellina v0.2", init_, tick, uninit )
+
+	fmt.Println("btc: " + btcusd)
+	fmt.Println("eth: " + ethusd)
 }
