69026f87733d8a8348566b8caaa85949a307b393
Wade <austinx.h.wade@intel.com>
2016-08-11 17:42:18 -0700

    Replace go-gl with Nanovg

--- cryptobox.go
+++ cryptobox.go
@@ -1,157 +1,121 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
-	"runtime"
-	"log"
 
-	"github.com/go-gl/gl/v2.1/gl"
-	"github.com/go-gl/glfw/v3.2/glfw"
+	"github.com/goxjs/gl"
+	"github.com/goxjs/glfw"
+	"github.com/shibukawa/nanovgo"
 )
 
-func init() {
-	// GLFW event handling must run on the main OS thread
-	runtime.LockOSThread()
-}
+var blowup bool
+var premult bool
 
 func main() {
-	/*json := getJSONFromApi()
+	//json := getJSONFromApi()
 
-	ethusd, btcusd := getCryptoValues(json)
+	ethusd, btcusd := "1", "2"//getCryptoValues(json)
 
 	fmt.Println("btc: " + btcusd)
-	fmt.Println("eth: " + ethusd)	*/
+	fmt.Println("eth: " + ethusd)
 
-	if err := glfw.Init(); err != nil {
-		log.Fatalln("failed to initialize glfw:", err)
-	}
-
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 2)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	window, err := glfw.CreateWindow(250, 150, "foo", nil, nil)
+	err := glfw.Init(gl.ContextWatcher)
 	if err != nil {
 		panic(err)
 	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Samples, 4)
+
+	window, err := glfw.CreateWindow(1000, 600, "NanoVGo", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.SetKeyCallback(key)
 	window.MakeContextCurrent()
 
-	if err := gl.Init(); err != nil {
+	ctx, err := nanovgo.NewContext(nanovgo.AntiAlias /*nanovgo.AntiAlias | nanovgo.StencilStrokes | nanovgo.Debug*/)
+	defer ctx.Delete()
+
+	font := ctx.CreateFont("sans", "github.com/austinwade/cryptobox/Roboto-Regular.ttf")
+
+	if font < 0 {
+		panic("Could not find font")
+	}
+
+	if err != nil {
 		panic(err)
 	}
 
-	setupScene()
+	glfw.SwapInterval(0)
+
 	for !window.ShouldClose() {
-		drawScene()
+		//t, _ := fps.UpdateGraph()
+
+		fbWidth, fbHeight := window.GetFramebufferSize()
+		winWidth, winHeight := window.GetSize()
+		//mx, my := window.GetCursorPos()
+
+		gl.Viewport(0, 0, fbWidth, fbHeight)
+		if premult {
+			gl.ClearColor(0, 0, 0, 0)
+		} else {
+			gl.ClearColor(1, 1, 1, 1)
+		}
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
+		gl.Enable(gl.BLEND)
+		gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+		gl.Enable(gl.CULL_FACE)
+		gl.Disable(gl.DEPTH_TEST)
+
+		ctx.BeginFrame(winWidth, winHeight, 1)
+
+		x, y := float32(30), float32(20)
+
+		ctx.BeginPath()
+
+		ctx.SetFontSize(150.0)
+		ctx.SetFontFace("sans")
+
+		ctx.SetTextAlign(nanovgo.AlignLeft | nanovgo.AlignMiddle)
+
+		ctx.SetFontBlur(2)
+		ctx.SetFillColor(nanovgo.RGBA(0, 0, 0, 255))
+		ctx.Text(x+50,y+75, "$ " + ethusd)
+
+		ctx.SetFontBlur(0)
+		ctx.SetFillColor(nanovgo.RGBA(0, 0, 0, 255))
+		ctx.Text(x+50,y+75, "$ " + btcusd)
+
+		ctx.EndFrame()
+
+		gl.Enable(gl.DEPTH_TEST)
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
-
 }
 
-func setupScene() {
-	gl.Enable(gl.DEPTH_TEST)
-	gl.Enable(gl.LIGHTING)
-
-	gl.ClearColor(0.5, 0.5, 0.5, 0.0)
-	gl.ClearDepth(1)
-	gl.DepthFunc(gl.LEQUAL)
-
-	ambient := []float32{0.5, 0.5, 0.5, 1}
-	diffuse := []float32{1, 1, 1, 1}
-	lightPosition := []float32{-5, 5, 10, 0}
-	gl.Lightfv(gl.LIGHT0, gl.AMBIENT, &ambient[0])
-	gl.Lightfv(gl.LIGHT0, gl.DIFFUSE, &diffuse[0])
-	gl.Lightfv(gl.LIGHT0, gl.POSITION, &lightPosition[0])
-	gl.Enable(gl.LIGHT0)
-
-	gl.MatrixMode(gl.PROJECTION)
-	gl.LoadIdentity()
-	gl.Frustum(-1, 1, -1, 1, 1.0, 10.0)
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
-}
-
-func drawScene() {
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
-	gl.Translatef(0, 0, -3.0)
-
-	gl.Color4f(1, 1, 1, 1)
-
-	gl.Begin(gl.QUADS)
-
-	gl.Normal3f(0, 0, 1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(-1, -1, 1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(1, -1, 1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(1, 1, 1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(-1, 1, 1)
-
-	gl.Normal3f(0, 0, -1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(-1, -1, -1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(-1, 1, -1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(1, 1, -1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(1, -1, -1)
-
-	gl.Normal3f(0, 1, 0)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(-1, 1, -1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(-1, 1, 1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(1, 1, 1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(1, 1, -1)
-
-	gl.Normal3f(0, -1, 0)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(-1, -1, -1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(1, -1, -1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(1, -1, 1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(-1, -1, 1)
-
-	gl.Normal3f(1, 0, 0)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(1, -1, -1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(1, 1, -1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(1, 1, 1)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(1, -1, 1)
-
-	gl.Normal3f(-1, 0, 0)
-	gl.TexCoord2f(0, 0)
-	gl.Vertex3f(-1, -1, -1)
-	gl.TexCoord2f(1, 0)
-	gl.Vertex3f(-1, -1, 1)
-	gl.TexCoord2f(1, 1)
-	gl.Vertex3f(-1, 1, 1)
-	gl.TexCoord2f(0, 1)
-	gl.Vertex3f(-1, 1, -1)
-
-	gl.End()
+func key(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	if key == glfw.KeyEscape && action == glfw.Press {
+		w.SetShouldClose(true)
+	} else if key == glfw.KeySpace && action == glfw.Press {
+		blowup = !blowup
+	} else if key == glfw.KeyP && action == glfw.Press {
+		premult = !premult
+	}
 }
 
 func getJSONFromApi() string {
-	var apiUrl string = "https://api.etherscan.io/api?module=stats&action=ethprice"
+	var apiUrl string = "http://api.etherscan.io/api?module=stats&action=ethprice"
 
-    resp, _ := http.Get(apiUrl)
+    	resp, err := http.Get(apiUrl)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
